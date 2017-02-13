78aefced7e833c61f968c056c6975c88708758a7
preetigr <preeti4gr@gmail.com>
2017-02-13 17:31:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 
 var element = document.getElementById('main-text');
 element.innerHTML = 'bon jour';
+
+img.onclick= fuction () {
+    var interval= setInterval(moveRight, 50);
+    
+};
