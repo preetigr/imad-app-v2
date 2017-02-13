50c6b03ebbc0c3a2846d13785cf8c771545d0975
preetigr <preeti4gr@gmail.com>
2017-02-13 17:18:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var element = document.getElementById('main-text');
+var element= document.getElementById('main-text');
 element.innerHTML= 'Bon Jour';
 
 var img= document.getElementById('madi');
