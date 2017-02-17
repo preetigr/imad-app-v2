be18b2f2e38c3ed2c0135ab52aef2244a0bac952
preetigr <preeti4gr@gmail.com>
2017-02-17 23:28:26 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,15 @@
-console.log('Loaded!');
-
-var element = document.getElementById('main-text');
-element.innerHTML = 'bon jour';
-
-var img= document.getElementById('madi');
-var marginLeft = 0;
-function moveRight () {
-    marginLeft = marginLeft + 1;
-    img.style.marginLeft = marginLeft + 'px';
-}
-img.onclick = function () {
-    var interval = setInterval(moveRight, 50);
-   };
+var button = document.getElementById('counter');
+button.onclick = function () {
+    var req = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+        if(request.readystate ==XMLHttpRequest.Done) {
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+    request.open('GET', 'http//:preetigr.imad.hasura-app.io/counter', true);
+  request.send(null);  
+};
