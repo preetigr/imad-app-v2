a2033a5aaf91dd844a094ac20a2b92c7eb2f6244
preetigr <preeti4gr@gmail.com>
2017-02-20 23:01:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 var button = document.getElementById('counter');
-var counter = 0;
+
 button.onclick = function () {
-    var req = new XMLHttpRequest();
+    var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
         if(request.readystate ==XMLHttpRequest.Done) {
             if (request.status === 200) {
