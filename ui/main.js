cf46d01ed02d05023fa962ece9c8dc21e4e907b7
preetigr <preeti4gr@gmail.com>
2017-02-21 19:55:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,7 +16,7 @@ button.onclick = function () {
 };
 
 var nameinput = document.getElementById('name');
-var name = nameInput.value;
+var names = nameInput.value;
 var submit = document.getElementById('submit_btn');
 submit.onclick = function () {
     
