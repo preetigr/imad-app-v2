a920465b84511574f16490c589b32173230af246
preetigr <preeti4gr@gmail.com>
2017-02-21 20:02:10 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -37,7 +37,7 @@ submit.onclick = function () {
             }
         };
         
-        var nameinput = document.getElementById('name');
+        var nameInput = document.getElementById('name');
 var name = nameInput.value;
 
     request.open('GET', 'http//:preetigr.imad.hasura-app.io/submit-name?name=' + name, true);
